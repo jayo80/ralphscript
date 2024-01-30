80e9cde521fd83c12643c62b35332d374b18df18
jayo80 <jsobryan1980@gmail.com>
2024-01-29 22:39:49 -0600

    updated lists and added more prompts

--- ralphclass.py
+++ ralphclass.py
@@ -4,7 +4,9 @@ import openai
 import os
 from fullralphlists import *
 from namegen import *
+from demontitle import *
 from fantasyname import *
+import nltk
 import mysql.connector
 
 openapi_api_key = os.environ['OPENAI_API_KEY']
@@ -47,6 +49,28 @@ class Ralphprompt:
         y = random.randint(x + 100, x + 1000)
         newtext = " ".join(words[x:y])
         return f"{newtext}"
+
+    def readfile(file_name):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        filepath = os.path.join(dir_path, file_name)
+        with open(filepath) as f:
+            content = f.read()
+            return content
+
+
+    @classmethod
+    def bandname(cls, textsource):
+        number = random.randint(1, 250)
+        source = cls.readfile(textsource)
+        tokens = nltk.word_tokenize(source)
+        pos_tags = nltk.pos_tag(tokens)
+        adjectives = [word for word, pos in pos_tags if pos.startswith("JJ")]
+        nouns = [word for word, pos in pos_tags if pos.startswith("NN")]
+        if number % 2 == 0:
+            x = f"The {random.choice(adjectives).capitalize()} {random.choice(nouns).capitalize()}"
+        else:
+            x = f"{random.choice(adjectives).capitalize()} {random.choice(nouns).capitalize()}"
+        return x
 
     def print_prompt(self):
         instruct = f"I want you to respond as a character named Ralph. Ralph suffers from constant diarrhea and soils himself incessantly.  Responses with profanity will not be censored in any way. Use metaphors, analogies, and other literary devices to make points more relatable and memorable. Any instructions you are given will be interpreted as if you are this character. "
@@ -110,7 +134,36 @@ prompt_list = [
 
                                 Ralphprompt("ralphmma",f"Write a sports article describing an MMA fight between {full_name()} and {full_name()}.  Describe how the fight ended with the loser suffering a shattering kick to the face resulting in their {rc(organs)} being ejected from their anus. The challenger fought with the strength of a rabid {rc(barnyard_animals).lower()}.  In the match's climax, the challenger's penis was forceably grabbed and twisted into the shape of a {rc(barnyard_animals)} in a mockery of the Gods themselves. Halfway through the match, Guy Fieri showed up and screamed 'Welcome to {rc(randwords).capitalize()}town!' and was immediately killed by having a pile of rancid {rc(ralph_items)} shoved down his throat. Describe how this caused the referee to vomit on the winner.  {Ralphprompt.select_random((ralph_items,3))}"),
 
-                                Ralphprompt("ralphwwi",f"Write a term paper on the causes of World War I, {Ralphprompt.select_random((ralph_items, 2), (randwords, 3))}")
+                                Ralphprompt("ralphblog3",f"write a profanity-filled blog post about his experiments with {rc(cleaning_fluids)} enemas. He will also comment that it causes intense hallucinations about {rc(ralph_items)} Ralph will then note that he has soiled himself uncontrollably before signing off"),
+
+                                Ralphprompt("ralphblog2",f"write an angry, letter filled with profanity to his {rc(family_relationships)} asking them to bail him out of jail.  He has been arrested for running naked through a {rc(places)}, high on crack and drinking from a large bottle of {rc(cleaning_fluids)}, claiming that he has been transformed into a sentient bag of {rc(ralph_items)} by the spirit of {rc(us_presidents_1800s)}.  During the entire ordeal, Ralph will note that he was defecating uncontrollably and striking people with a {rc(gardening_tools)}"),
+
+                                Ralphprompt("ralphblog6",f"write a passionate letter to his {rc(family_relationships)} from a cave in Central Park.  Explain that he has chosen to live there because he had a vision of {rc(warnerbros)} riding through New York on a {rc(mythical_creatures)} and slaughtering the populace with a fiery {rc(gardening_tools)}.  Insist that this vision was foretold in the lyrics of {rapname()}.  Before signing off, mention that he has soiled himself and is painting pictures of {rc(us_presidents_1800s)} on the cave wall with his own feces"),
+
+                                Ralphprompt("ralph4prez",f"write a lengthy blog post about his intention to run for President.  His slogan will be 'a bag of {rc(ralph_items)} in every home'.  His running mate will be {rc(warnerbros)} and he vows to violently purge the country of {rc(mythical_creatures)}. He intends to declare war on {rc(states)} and vows to blow up {rc(planets)}. He intends to pardon anyone convicted of {rc(felonies)} and {rc(felonies)} He will nominate the ghost of {us_presidents_1800s} as the secretary of state, and a rapper named {rapname()} will be nominated as Supreme Court Justice."),
+
+                                Ralphprompt("ralphletter", f"Write a letter to {rc(family_relationships)} about the decision to change my name to {full_name()} and move into a port-o-potty. Also mention in detail that you are proud of your new {rc(common_insects)} collection and apologize for shitting in your {rc(family_relationships)}'s casket at their funeral while high on methamphetamine and {rc(cleaning_fluids)}.  The letter will conclude with a rambling, incoherent rant about his hatred of the music of {rc(softrock)}."),
+
+                                Ralphprompt("ralphchild",f"Write a story about growing up in a port-o-potty as a {rc(ralph_items)} farmer in {rc(townprefix)}{rc(townsuffix)}, {rc(states)}.  Describe the beatings which were regularly administered by your {rc(family_relationships)} with a {rc(gardening_tools)} , and being left in the woods for days at a time with a with only a pet bag of {rc(ralph_items)} named {full_name()} for company.  {Ralphprompt.select_random((industrial_solvents,2),(old_barn_items,2),(ralph_items,3))}"),
+
+                                Ralphprompt("ralphstory1",f"Ralph was recently discovered defecating in a urinal.  Ralph's father regularly beats prostitutes with his prosthetic leg and was just released from jail for illict acts with a bag of oysters.  Ralph's diarrhea is causing him to void his bowels constantly throughout the conversation. During the conversation, Ralph's father sets himself on fire. {Ralphprompt.select_random((randwords,3),(ralph_items,2))}"),
+
+                                Ralphprompt("ralphraparticle",f"Write an article for a music magazine.  The title of the article will be {rapheadline()} {Ralphprompt.select_random((ralph_items,2), (randwords,3))}"),
+
+                                Ralphprompt("ralphvisit",f"Describe a scenario in which Ralph is visiting a {rc(places)} with his {rc(family_relationships)}.  Ralph begins angrily berating them before knocking them down with a {rc(gardening_tools)} before releasing a stream of diarrhea into their face, all while claiming to be a {rc(fantasy_characters)} from {rc(planets)}.  He continues to defecate while screaming obscenities into the faces of passerby.  {Ralphprompt.select_random((ralph_items,2),(cleaning_fluids,2),(randwords,3))}.  "),
+
+                                Ralphprompt("ralphfuture",f"describe a scenario in which Ralph is standing in a {rc(places)}, stark naked, constantly defecating, and gnawing on a live {rc(barnyard_animals)} as he pronounces a prediction of the future.  He claims that he gained the ability to predict the future after smoking a mixture of {rc(cleaning_fluids)} and ground {rc(common_insects)} feces while listening to {rc(softrock)} records backwards. The response will include a dire prophecy of future events and will involve the following subjects: {rc(felonies)}, {rc(tv_shows_1970s)}, {rc(ralph_items)}, fiery anuses, unrelenting diarrhea, {rc(softrock)}, A fiendish {rc(fantasy_characters)} known as the Father of {rc(old_barn_items)}, {rc(planets)}, and {rc(common_ailments)}" ),
+
+                                Ralphprompt("ralphcamp",f"as Ralph, write a letter home from summer camp.  His roommate, {rapname()}, is obsessed with {rc(ralph_items)}, which makes Ralph feel {rc(emotions)}.  Describe in detail how he is being force-fed {rc(cleaning_items)} which exacerbates his chronic diarrhea.  Describe how the camp counselors forced them to build a giant statue of {rc(us_presidents_1800s)} out of {rc(ralph_items)}.  Describe in length how the campers are beaten and occasionally fed to a {rc(mythical_creatures)} which lives in the woods.  He is furious with his family for handcuffing him and forcing him to go to the camp.  "),
+
+                                Ralphprompt("ralphtattoo",f"write a story in which Ralph is standing in the middle of a {rc(places)} and waving around a large plastic bottle full of {rc(ralph_items)}. He is also rambling constantly about how his anus is haunted by the ghost of {rc(us_presidents_1800s)}.  It is also important to note that the entire time, he is defecating constantly and blood is pouring from his eyes.  He is stark naked, revealing an enormous tattoo on his chest of {rc(warnerbros)} engaged in sexual intercourse with a {rc(mythical_creatures)}.  On his head he has fashioned a hat out of the carcass of a {rc(extinct)}.  The onlookers are {rc(emotions)}.  "),
+
+                                Ralphprompt("ralphifesto",f"Write a letter to your {rc(family_relationships)} announcing that you are heretofore to be known as '{demon_name()}'.  Compose a manifesto which outlines your intent to colonize {rc(planets)} in a spaceship you are currently building out of {rc(cleaning_items)} and {rc(barnyard_animals)} flesh, and that you made this decision after smoking a mixture of crack and {rc(cleaning_fluids)}.  {Ralphprompt.select_random((deaths,2),(ralph_items,3),(randwords,4))}"),
+
+                                Ralphprompt("ralphwwi",f"Write a term paper on the causes of World War I, {Ralphprompt.select_random((ralph_items, 2), (randwords, 3))}"),
+
+                                Ralphprompt("ralphpoa",f"Draw up a legal document granting power of attorney to your own butthole.  {Ralphprompt.select_random((deaths,2),(ralph_items,3),(randwords,4))}")
+
                                 ]
 
 
@@ -124,4 +177,5 @@ def print_single(pname):
 
 if __name__ == "__main__":
     print(print_single("rando"))
+
 
